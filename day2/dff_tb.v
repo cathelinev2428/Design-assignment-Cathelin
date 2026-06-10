52282cc4b60acd8c89be06f5bd485f7ec0c30276
cathelinev2428 <cathelin.ev2428@saintgits.org>
2026-06-10 14:15:41 +0530

    Add files via upload

---
+++ day2/dff_tb.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2026 15:36:19
+// Design Name: 
+// Module Name: dff_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dff_tb();
+
+reg d_tb,rst_tb,clk_tb;
+wire q_tb,qbar_tb;
+
+dff dut(d_tb,rst_tb,clk_tb,q_tb,qbar_tb);
+
+initial
+begin
+    {d_tb,rst_tb,clk_tb}=0;
+end
+
+always #5 clk_tb=~clk_tb;
+
+initial
+begin
+    rst_tb=1;
+    #10;
+
+    rst_tb=0;
+    d_tb=0;
+    #10;
+
+    d_tb=1;
+    #10;
+
+    d_tb=0;
+    #10;
+
+    d_tb=1;
+    #10;
+end
+endmodule
